a5248d487861dd3b72cbafa9dbf35165e78d74d3
cmok4290 <cmok4290@gmail.com>
2018-10-27 15:45:31 -0400

    created and trained svm classifier

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,8 +23,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+import numpy as np
 
+# create model
+clf = SVC(C=10000, kernel="rbf", gamma="auto")
+
+# fit training data to model
+clf.fit(features_train, labels_train)
+
+# predict with test data
+pred = clf.predict(features_test)
+print "email 10:", pred[10]
+print "email 26:", pred[26]
+print "email 50:", pred[50]
+# print "email 100:", pred[100]
+
+# print number of predictions for Chris(1)
+pred_arr = np.array(pred)
+print "no. of predictions for Chris: ", np.sum(pred_arr)
+
+# print accuracy score
+print clf.score(features_test, labels_test)
 #########################################################
 
 

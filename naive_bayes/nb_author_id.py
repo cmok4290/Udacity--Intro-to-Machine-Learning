103695bea5f422eface31c91959223a604f8e3fb
cmok4290 <cmok4290@gmail.com>
2018-10-27 12:59:15 -0400

    created and trained naive bayes classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -25,8 +25,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+
+# create model
+clf = GaussianNB()
+
+# fit training data to model
+clf.fit(features_train, labels_train)
+
+# predict with test data
+clf.predict(features_test)
+
+# print accuracy score
+print clf.score(features_test, labels_test)
 
 #########################################################
 
